5ebf24328e19dd2df045dff00e2ea06c799c752d
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2020-09-27 10:00:03 +0900

    bug fix

--- train.sh
+++ train.sh
@@ -1,5 +1,7 @@
 #! /bin/sh
 udpipe --train --tokenizer=epochs=30 --tagger=iterations=20 --parser='embedding_form=10;embedding_lemma=0;embedding_upostag=10;embedding_xpostag=30;embedding_feats=0;embedding_deprel=20;iterations=20' ud-thai.udpipe th_pud-ud-orchid.conllu
-mv spacy_thai/ud-thai.udpipe spacy_thai/ud-thai.udpipe.old
-mv ud-thai.udpipe spacy_thai/ud-thai.udpipe
+if [ -s ud-thai.udpipe ]
+then mv -f spacy_thai/ud-thai.udpipe spacy_thai/ud-thai.udpipe.old
+     mv ud-thai.udpipe spacy_thai/ud-thai.udpipe
+fi
 exit 0
