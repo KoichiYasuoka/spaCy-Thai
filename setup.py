8e4d2e3f84af68a0e09572c16166cbbc8563bb45
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2020-09-27 09:38:13 +0900

    version 0.4.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ URL="https://github.com/KoichiYasuoka/spaCy-Thai"
 
 setuptools.setup(
   name="spacy_thai",
-  version="0.4.2",
+  version="0.4.3",
   description="Dependency-parser for Thai language",
   long_description=long_description,
   long_description_content_type="text/markdown",
