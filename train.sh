2bd3f253232c5e8896d3fbc7919f988ac1563aa9
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2020-09-27 09:37:53 +0900

    model improved

--- train.sh
+++ train.sh
@@ -1,4 +1,5 @@
 #! /bin/sh
 udpipe --train --tokenizer=epochs=30 --tagger=iterations=20 --parser='embedding_form=10;embedding_lemma=0;embedding_upostag=10;embedding_xpostag=30;embedding_feats=0;embedding_deprel=20;iterations=20' ud-thai.udpipe th_pud-ud-orchid.conllu
-# mv -i ud-thai.udpipe spacy_thai/.
+mv spacy_thai/ud-thai.udpipe spacy_thai/ud-thai.udpipe.old
+mv ud-thai.udpipe spacy_thai/ud-thai.udpipe
 exit 0
